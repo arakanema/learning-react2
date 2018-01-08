0db15d52a639a403f4e5130f22ac28d45d067fec
Masahiro Arakane <arakane.masahiro@gmail.com>
2018-01-09 00:38:06 +0900

    Multi-Page-Feeling in a Single-Page-App: Routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import axios from 'axios';
 import axios from '../../axios';
 
@@ -8,55 +8,61 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false,
-  };
-
-  componentDidMount() {
-    axios.get('/posts').then(response => {
-      const posts = response.data.slice(0, 4);
-      const updatedPosts = posts.map(post => {
-        return {...post, author: 'MA'}; // jsonデータに項目追加するパターン
-      });
-      this.setState({posts: updatedPosts});
-      // console.log(response);
-    }).catch(error => {
-      // console.log(error);
-      this.setState({error: true});
-    });
-  }
-
-  postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id});
-  };
-
-  render() {
-    let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-    if (!this.state.error) {
-      posts = this.state.posts.map(post => {
-        return <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}/>;
-      });
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false
     }
-    return (
-        <div>
-          <section className="Posts">
-            {posts}
-          </section>
-          <section>
-            <FullPost id={this.state.selectedPostId}/>
-          </section>
-          <section>
-            <NewPost/>
-          </section>
-        </div>
-    );
-  }
+
+    componentDidMount () {
+        axios.get( '/posts' )
+            .then( response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+                // console.log( response );
+            } )
+            .catch(error => {
+                // console.log(error);
+                this.setState({error: true});
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+    render () {
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />;
+            });
+        }
+
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostId} />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
 }
 
 export default Blog;
